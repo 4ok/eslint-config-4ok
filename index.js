ec433c1d1b29d92215afc6c9abfd8354abfaa06b
lipolyakov <lipolyakov@yandex-team.ru>
2016-04-07 01:43:44 +0300

    Refactoring

--- index.js
+++ index.js
@@ -1,16 +1,1 @@
-module.exports = {
-    extends : 'airbnb/base',
-    parserOptions : {
-        ecmaVersion : 6
-    },
-    rules : {
-        indent : [2, 4, {SwitchCase : 1}],
-        'newline-before-return' : 2,
-        'no-param-reassign' : 0,
-        'space-before-blocks' : 2,
-        'key-spacing' : [2, {beforeColon : true, afterColon : true}],
-        'newline-after-var' : 2,
-        'padded-blocks' : 0,
-        'prefer-template' : 0
-    }
-};
+module.exports = require('./eslintrc.json');
