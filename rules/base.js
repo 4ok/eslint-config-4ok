f1d3ab217add68b5356c787582bbc2ed36a9e735
lipolyakov <lipolyakov@yandex-team.ru>
2017-10-18 06:00:13 +0300

    Fixed rules

--- rules/base.js
+++ rules/base.js
@@ -3,16 +3,23 @@ module.exports = {
         ecmaVersion: 6,
     },
     rules: {
+        'comma-dangle': [2, {
+            arrays: 'always-multiline',
+            objects: 'always-multiline',
+            imports: 'always-multiline',
+            exports: 'always-multiline',
+            functions: 'never',
+        }],
         indent: [2, 4, {
             SwitchCase: 1
         }],
+        'newline-after-var': 2,
         'newline-before-return': 2,
         'no-param-reassign': 0,
-        'newline-after-var': 2,
-        'padded-blocks': 0,
-        'prefer-template': 0,
         'no-underscore-dangle': [2, {
             allowAfterThis: true
         }],
-    }
+        'padded-blocks': 0,
+        'prefer-template': 0,
+    },
 };
