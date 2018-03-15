495ab79555bdea8cc69f868e407edcd56fb13d1d
lipolyakov <lipolyakov@yandex-team.ru>
2018-03-15 18:38:02 +0300

    New codestyle

--- rules/base.js
+++ rules/base.js
@@ -1,7 +1,5 @@
 module.exports = {
-	parserOptions: {
-		ecmaVersion: 6,
-	},
+	parserOptions: { ecmaVersion: 6 },
 	rules: {
 		'comma-dangle': [ 2, {
 			arrays: 'always-multiline',
@@ -11,27 +9,30 @@ module.exports = {
 			functions: 'never',
 		}],
 		'no-tabs': 0,
-		indent: [ 2, 'tab', {
-			SwitchCase: 1,
-		}],
+		indent: [ 2, 'tab', { SwitchCase: 1 }],
 		'newline-after-var': 2,
 		'newline-before-return': 2,
 		'no-param-reassign': 0,
-		'no-underscore-dangle': [ 2, {
-			allowAfterThis: true,
-		}],
+		'no-underscore-dangle': [ 2, { allowAfterThis: true }],
 		'padded-blocks': 0,
-		'no-plusplus': [ 2, {
-			allowForLoopAfterthoughts: true,
-		}],
+		'no-plusplus': [ 2, { allowForLoopAfterthoughts: true }],
 		'template-curly-spacing': [ 2, 'always' ],
 		'array-bracket-spacing': [ 2, 'always', {
 			objectsInArrays: false,
 			arraysInArrays: false,
 		}],
-		'newline-per-chained-call': [ 2, {
-			ignoreChainWithDepth: 2,
-		}],
+		'newline-per-chained-call': [ 2, { ignoreChainWithDepth: 2 }],
 		semi: [ 2, 'never' ],
+		'object-curly-newline': [ 2, {
+			ObjectExpression: {
+				multiline: true,
+				minProperties: 2,
+			},
+			ObjectPattern: {
+				multiline: true,
+				minProperties: 4,
+			},
+		}],
+		'object-property-newline': [ 2, { allowAllPropertiesOnSameLine: false }],
 	},
 }
